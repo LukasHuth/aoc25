f6dda7bb1a4fde335f0bbb7fb7b9328238baa759
Lukas Huth <60547284+LukasHuth@users.noreply.github.com>
2026-02-21 22:19:13 +0100

    Start with day 11

---
+++ day11/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let part = std::env::args().skip(1).next().unwrap_or("1".to_string());
+    match part.as_str() {
+        "1" => part1::part1(),
+        "2" => part2::part2(),
+        _ => panic!("please select part 1 or 2"),
+    }
+}
+mod part1;
+mod part2;
