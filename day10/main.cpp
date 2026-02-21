2b597c647518c80c1853f3aa3c40e9cdd7b9ffac
LukasHuth <60547284+LukasHuth@users.noreply.github.com>
2026-02-21 22:19:05 +0100

    Move to string_view and "lookup" table

--- day10/main.cpp
+++ day10/main.cpp
@@ -4,7 +4,10 @@
 #include <queue>
 #include <stdlib.h>
 #include <string>
+#include <string_view>
 #include <tuple>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 class Machine {
@@ -15,18 +18,18 @@ private:
 
 public:
   uint32_t findMoves();
-  Machine(std::string);
+  Machine(std::string_view);
 };
 
 void part1() {
   std::ifstream InputFile("input.txt");
   std::string input;
   std::vector<Machine> machines;
-  while(std::getline(InputFile, input)) {
+  while (std::getline(InputFile, input)) {
     machines.push_back(Machine(input));
   }
   uint32_t result = 0;
-  for(auto machine : machines) {
+  for (auto machine : machines) {
     result += machine.findMoves();
   }
   std::cout << result << std::endl;
@@ -43,12 +46,33 @@ int main(int argc, char **argv) {
   return 0;
 }
 
+struct TupleHash {
+  std::size_t
+  operator()(const std::tuple<uint32_t, uint32_t, uint32_t> &t) const {
+    auto [a, b, c] = t;
+
+    std::size_t h1 = std::hash<unsigned>{}(a);
+    std::size_t h2 = std::hash<unsigned>{}(b);
+    std::size_t h3 = std::hash<unsigned>{}(c);
+
+    // Good hash combine
+    std::size_t seed = h1;
+    seed ^= h2 + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    seed ^= h3 + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+
+    return seed;
+  }
+};
+
 uint32_t Machine::findMoves() {
+  std::unordered_set<std::tuple<uint32_t, uint32_t, uint32_t>, TupleHash>
+      lookup_set;
   // std::cout << "start find move" << std::endl;
   uint32_t result = 0;
   // state, modification, depth
   std::queue<std::tuple<uint32_t, uint32_t, uint32_t>> moves;
   for (uint32_t modification : this->buttonWiring) {
+    lookup_set.insert({0, modification, 1});
     moves.push({0, modification, 1});
   }
   while (!moves.empty()) {
@@ -57,56 +81,57 @@ uint32_t Machine::findMoves() {
     uint32_t state = std::get<0>(entry);
     uint32_t modifier = std::get<1>(entry);
     uint32_t depth = std::get<2>(entry);
-    // std::cout << "state: " << state << " modifier: " << modifier << " depth: " << depth << std::endl;
+    // std::cout << "state: " << state << " modifier: " << modifier << " depth:
+    // " << depth << std::endl;
     state ^= modifier;
-    if(state == this->expected_lights) return depth;
+    if (state == this->expected_lights)
+      return depth;
     depth++;
     for (uint32_t modification : this->buttonWiring) {
+      if (!lookup_set.insert({state, modification, depth}).second)
+        continue;
       moves.push({state, modification, depth});
     }
   }
   return result;
 }
 
-uint32_t getButtonMask(std::string button) {
+uint32_t getButtonMask(std::string_view button) {
   uint32_t result = 0;
-  std::string light;
-  int32_t comma_split = button.find(',');
-  while (comma_split > -1) {
-    light = button.substr(0, comma_split);
-    uint8_t pos = std::stoi(light);
-    result |= 1 << pos;
-    button = button.substr(comma_split + 1);
-    comma_split = button.find(',');
+  while (!button.empty()) {
+    size_t pos = button.find(',');
+    std::string_view target = button.substr(0, pos);
+    uint8_t offset = std::stoi(std::string(target));
+    result |= 1 << offset;
+    if (pos == std::string_view::npos)
+      break;
+    button.remove_prefix(pos + 1);
   }
-  uint8_t pos = std::stoi(button);
-  result |= 1 << pos;
   return result;
 }
 
-Machine::Machine(std::string input) {
+Machine::Machine(std::string_view input) {
   int32_t light_split = input.find(' ');
-  std::string lights = input.substr(0, light_split);
+  std::string_view lights = input.substr(0, light_split);
   int32_t joltage_split = input.find_last_of(' ');
-  std::string buttons = input.substr(light_split + 1, joltage_split - light_split - 1);
-  std::string joltage = input.substr(joltage_split);
+  std::string_view buttons =
+      input.substr(light_split + 1, joltage_split - light_split - 1);
+  // std::string_view joltage = input.substr(joltage_split);
   this->expected_lights = 0;
   uint8_t i = 0;
-  for(char c : lights) {
-    if(c != '.' && c != '#') continue;
-    if(c == '#'){
+  for (char c : lights) {
+    if (c != '.' && c != '#')
+      continue;
+    if (c == '#') {
       this->expected_lights |= 1 << i;
     }
     i++;
   }
-  int32_t space_split = buttons.find(' ');
-  std::string button;
-  while(space_split > -1) {
-    button = buttons.substr(1, space_split-1);
-    buttons = buttons.substr(space_split + 1);
-    this->buttonWiring.push_back(getButtonMask(button));
-    space_split = buttons.find(' ');
+  while (!buttons.empty()) {
+    size_t pos = buttons.find(' ');
+    this->buttonWiring.push_back(getButtonMask(buttons.substr(1, pos - 1)));
+    if (pos == std::string_view::npos)
+      break;
+    buttons.remove_prefix(pos + 1);
   }
-  button = buttons.substr(1, buttons.length() - 2);
-  this->buttonWiring.push_back(getButtonMask(button));
 }
