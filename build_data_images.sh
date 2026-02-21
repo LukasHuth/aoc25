03a5bca8570d1a2052e90e59482056c484551898
Lukas Huth <60547284+LukasHuth@users.noreply.github.com>
2026-02-21 22:18:40 +0100

    Add day one

--- build_data_images.sh
+++ build_data_images.sh
@@ -3,13 +3,24 @@ set -eu
 
 export DOCKER_BUILDKIT=1
 
+tempfile=$(mktemp)
+trap 'rm -f "$tempfile"' EXIT
+
+if [ -z "${SESSION_TOKEN}" ]; then
+  echo "Missing SESSION_TOKEN"
+  exit 1
+fi
+
+echo "${SESSION_TOKEN}" > "${tempfile}"
+
 for DAY in $(seq 1 12); do
   echo "▶ Building day $DAY"
 
   docker buildx build \
     --load \
+    --no-cache \
     --build-arg DAY="$DAY" \
-    --secret id=session,env=SESSION_TOKEN \
+    --secret id=session,src="${tempfile}" \
     -t "aoc-data:day$DAY" \
     -f datapuller/Dockerfile \
     .
--- run_day.sh
+++ run_day.sh
@@ -7,7 +7,7 @@ test "$DAY" -ge 1 -a "$DAY" -le 12
 
 ./build_day.sh $DAY
 
-part1=$(docker run aoc-day:day${DAY}-part1)
+part1=$(docker run --rm aoc-day:day${DAY}-part1)
 echo "part1:  ${part1}"
-part2=$(docker run aoc-day:day${DAY}-part2)
+part2=$(docker run --rm aoc-day:day${DAY}-part2)
 echo "part2:  ${part2}"
